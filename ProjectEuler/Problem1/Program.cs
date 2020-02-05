ece212885126109736ae02fa3a3f38f7fa519c23
Yusuf Topkaya <yusuf-topkaya@hotmail.com>
2020-02-05 13:35:34 +0300

    First problem.

---
+++ ProjectEuler/Problem1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Find the sum of all the multiples of 3 or 5 below 1000.
+
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                sum += (i % 3 == 0 || i % 5 == 0) ? i : 0;
+            }
+            Console.Write(sum);
+            Console.Read();
+
+
+        }
+    }
+}
