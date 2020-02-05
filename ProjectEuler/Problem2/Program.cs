ecb4c99cf41fa00235ac54e4f904f61c648715fd
Yusuf Topkaya <yusuf-topkaya@hotmail.com>
2020-02-05 14:22:48 +0300

    Second problem.

---
+++ ProjectEuler/Problem2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Even Fibonacci Numbers
+            int sum = 0;
+            Func<int, int> fib = null;
+            fib = n => (n < 2) ? 1 : fib(n - 1) + fib(n - 2);
+            for (int i = 1; i < 100; i++)
+            {     
+                if(fib(i) > 4000000)
+                    break;
+                if (fib(i) % 2 == 0)
+                    sum += fib(i);
+            }
+            Console.WriteLine(sum);
+            Console.Read();
+
+        }
+    }
+}
