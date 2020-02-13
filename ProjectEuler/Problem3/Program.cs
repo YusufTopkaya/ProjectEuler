cefad48a58be50daec05bbbedb6305ffeae4fa37
Yusuf Topkaya <yusuf-topkaya@hotmail.com>
2020-02-13 15:52:20 +0300

    Problem 3.

---
+++ ProjectEuler/Problem3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //What is the largest prime factor of the number 600851475143?
+            long number = 600851475143;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0 && isPrime(i))
+                    Console.WriteLine(i);
+            }
+        }
+
+        public static bool isPrime(int number)
+        {
+            if (number == 1 || number == 2)
+                return true;
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+
+            return true;
+        }
+    }
+}
