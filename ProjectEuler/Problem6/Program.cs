6c3607349bcda52082700f5e57cf37dbd7d15bca
Yusuf Topkaya <yusuf-topkaya@hotmail.com>
2020-02-20 00:26:35 +0300

    Problem 7

--- ProjectEuler/Problem6/Program.cs
+++ ProjectEuler/Problem6/Program.cs
@@ -10,7 +10,8 @@ namespace Problem6
     {
         static void Main(string[] args)
         {
-            int sum=0;
+            //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+            int sum =0;
             int limit = 100;
             int sqrToLimit = (50 * 101) * (50 * 101);
             for (int i = 1; i <= 100; i++)
---
+++ ProjectEuler/Problem7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //What is the 10001st prime number?
+            int counter = 1;
+            int i = 3;
+            while (counter < 10001)
+            {
+
+                if (isPrime(i))
+                    counter++;
+                i++;
+            }
+            Console.Write(i - 1);
+            Console.Read();
+
+
+
+
+        }
+        public static bool isPrime(int number)
+        {
+
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+
+            return true;
+        }
+    }
+}
